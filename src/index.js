3dfd02694c7bd983b39baa60f47a4c3d3ad590c1
Abiatar Cosma <abiatar.cosma@yahoo.com>
2025-02-23 16:31:10 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const imageUrl =
+  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const productPrice = 10.99;
+
+const product = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="640" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {productPrice}$</p>
+    <button type="button">Add to cart</button>
+  </div>
 );
+
+ReactDOM.createRoot(document.getElementById("root")).render(product);
